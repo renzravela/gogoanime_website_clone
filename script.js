afc632104013561e00f488c97776b019c28b7632
renz_aerol_ravela <renzaerol.ravela@gmail.com>
2024-08-10 09:23:55 +0800

    project commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// function that display the menu content when click
+function myFunction() {
+    var x = document.getElementById("menu-content");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
